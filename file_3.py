925e150e5630f3282208acce6cd66dbe093192ed
_sunny_ <cloudtosunny@gmail.com>
2024-02-29 22:19:14 +0530

    edited

---
+++ file_3.py
@@ -0,0 +1,11 @@
+# nested if statements
+
+num=int(input("enter your desired no. : "))
+if (num < 0):
+    print("number is negative")
+elif (num == 0):
+    print("you got 0")
+else:
+    (num > 0)
+    print("number is positive")
+print("thanks for being here")
