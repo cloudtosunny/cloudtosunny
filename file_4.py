8c8617376808111e33ed0cbc6d08d7b4940fa053
_sunny_ <cloudtosunny@gmail.com>
2024-02-29 22:49:27 +0530

    divisibility

---
+++ file_4.py
@@ -0,0 +1,8 @@
+n=int(input("enter your desired no. "))
+if (n%2 == 0 and n%4==0):
+    print("your no. is divided by 4")
+elif (n%3 == 0):
+    print("your no. is divisible by 3")
+elif (n%2 == 0):
+    print("your number is divisible by 2")
+print("thanx for being here")
