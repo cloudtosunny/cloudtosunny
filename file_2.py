8b46f60df27fe4ec8bee646e08e24b596b545938
_sunny_ <cloudtosunny@gmail.com>
2024-02-29 22:05:54 +0530

    learning

---
+++ file_2.py
@@ -0,0 +1,7 @@
+appleprice=int(input("enter the price of apple: "))
+budget=int(input("what is your budget: "))
+if (budget-appleprice>=70):
+    print("you can buy the apples.")
+else:
+    print("you are advised not to buy apples")
+print("thanks for  being here")
